c5b66d1db4d5f01186138cc92b219209c735c94c
okidoki-jpg <okuhlen.20@gmail.com>
2022-10-18 12:47:57 +0200

    error handling

--- _printf.c
+++ _printf.c
@@ -14,7 +14,7 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
-	if (!format || (*format == '%' && *(!format + 1)))
+	if (!format || (*format == '%' && !*(format + 1)))
 		return (-1);
 	while (format[i])
 	{
