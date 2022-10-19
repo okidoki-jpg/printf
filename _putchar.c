d0f6b4f8b2301a60ba6546d03d6ff1ef081e0237
okidoki-jpg <okuhlen.20@gmail.com>
2022-10-19 16:15:30 +0200

    removed stdio.h from main. added _putchar comments.

--- _putchar.c
+++ _putchar.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _putchar - output a single character
+ * @c: character to output
+ *
+ * Return: 1 on success
+ */
+
 int _putchar(char c)
 {
 	write(1, &c, 1);
--- main.h
+++ main.h
@@ -2,7 +2,6 @@
 #define _main_h_
 
 #include <stdarg.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
