2cf1e04e89404f4c7331f010b5457c6444a0dbc0
okidoki-jpg <okuhlen.20@gmail.com>
2022-10-19 20:21:23 +0200

    running focused tests on task 0

--- misc.c
+++ misc.c
@@ -13,9 +13,8 @@ int f_id(char ch)
 
 	while (*c)
 	{
-		if (*c == ch)
+		if (*c++ == ch)
 			return (1);
-		c++;
 	}
 	return (0);
 }
@@ -32,9 +31,7 @@ int _strlen(char *str)
 	int i = 0;
 
 	while (str[i])
-	{
 		i++;
-	}
 	return (i);
 }
 
